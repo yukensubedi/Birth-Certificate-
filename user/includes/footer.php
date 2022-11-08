a5302a3f51bb449196bb7b71693697bc661fef99
Ukn Subedi <yukensubedi@gmail.com>
2022-11-08 21:56:34 +0545

    final changes

---
+++ user/includes/footer.php
@@ -0,0 +1,14 @@
+ <!-- Footer Start-->
+    <div class="footer-copyright-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copy-right">
+                    <p>Copyright &copy; <?= date("Y") ?> Online Birth Certificate 
+Management System.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End-->
